bb21c988853f4130579fe064e9b333b83759fbce
Samer Salib <samer.salib@intergraph.com>
2015-10-21 14:27:59 -0500

    Testing the first draft of the azuredeploy.json file.

---
+++ AzureVMsDeploy.ps1
@@ -0,0 +1,36 @@
+﻿switch-azureMode -Name AzureResourceManager
+
+# Gather user input
+$subscription = Read-Host -Prompt 'Input your subscription name: '
+$location = Read-Host -Prompt 'Input your prefered Azure regional location: '
+$adminUsername = Read-Host -Prompt 'Input an admin username for the new VM: '
+$adminPassword = Read-Host -Prompt 'Input an admin password for the new VM: '
+$dnsNameForPublicIP = Read-Host -Prompt 'Input a dns name for Public IP: '
+
+$resourceGroupName = "WebCAD"
+$resourceGroupDeploymentName = "WebCad Deployment"
+
+Add-AzureAccount
+
+Select-AzureSubscription
+
+New-AzureResourceGroup -Name $resourceGroupName -Location $location
+
+$Deployment = @{
+	Name = 'WebCAD_Deployment';
+	ResourceGroupName = 'WebCAD';
+	Mode = 'Complete';
+	TemplateFile = "$PSScriptRoot\AzureVMsDeploy.json";
+	TemplateParameterObject = @{
+		newStorageAccountName = 'WebCAD_Storage';
+		adminUsername = $adminUsername;
+		adminPassword = $adminPassword;
+		dnsNameForPublicIP = $dnsNameForPublicIP;
+	}
+}
+
+New-AzureResourceGroupDeployment @Deployment;
+
+
+
+
